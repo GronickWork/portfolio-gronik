eed0550e8c7aceba6e388e562fbfa6cd4058b323
Gronik4 <golegni@yandex.ru>
2026-04-08 22:08:35 +0300

    Add Logo component

--- src/components/Header/Logo/Logo.tsx
+++ src/components/Header/Logo/Logo.tsx
@@ -1,9 +1,17 @@
-import Foto from '../../../assets/images/svg/logo.svg';
+//import Foto from '../../../assets/images/svg/logo_file.svg';
+/*перенес все картинки в public - т.к. Файлы в этой папке не обрабатываются сборщиком. Они просто копируются в итоговую сборку (в папку build)
+ и доступны по прямому URL.
+ иначе вылетает ошибка: Module build failed (from ./node_modules/@svgr/webpack/lib/index.js):
+SyntaxError: unknown file: Namespace tags are not supported by default. React's JSX doesn't support namespace tags. 
+You can set `throwIfNamespace: false` to bypass this warning.
+*/
+import './logo.scss';
 
 export default function Logo() {
+  const imgLogo = 'images/svg/logo_file.svg';
   return (
     <div className="logo">
-      <img src={Foto} alt="Фото автора"/>
+      <img src={imgLogo} alt="Логотип"/>
       <h1>Олег Гречишников</h1>
     </div>
   )
