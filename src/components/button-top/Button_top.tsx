997bd36e68409dc960c21c5a046005cce148827a
Gronik4 <golegni@yandex.ru>
2026-04-29 21:44:21 +0300

    Corrected Button_top

--- src/components/button-top/Button_top.tsx
+++ src/components/button-top/Button_top.tsx
@@ -1,27 +1,45 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import './button_top.scss';
 
 const ButtonTop = () => {
-   useEffect(()=> {
-      window.addEventListener('scroll', handleScroll);
-      // Убираем слушатель при размонтировании компонента
-      return () => {
-        window.removeEventListener('scroll', handleScroll);
-      };
-    }, []);
+  const btnRef = useRef<HTMLDivElement>(null);
+  const handleScroll = ()=> {
+    if(btnRef.current) btnRef.current.hidden = (window.scrollY < window.innerHeight / 2);
+/*Заменили document.documentElement.clientHeight на window.innerHeight,
+  что является более современным и предпочтительным способом получения высоты видимой области окна.*/
+  }
 
-    function handleScroll(e: any) {
-        const btn = e.target;
-        if(btn) btn.hidden = (window.scrollY < window.innerHeight / 2);
-        /*Заменили document.documentElement.clientHeight на window.innerHeight,
-          что является более современным и предпочтительным способом получения высоты видимой области окна.*/
-      }
+  function smoothlyTop(e: any) {
+    e.preventDefault();
+    const top = document.getElementById('top');
+    top?.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  }
+  
+  useEffect(()=> {
+    window.addEventListener('scroll', handleScroll);
+    handleScroll();
+    // Убираем слушатель при размонтировании компонента
+    return () => {
+    window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+    
   
   return (
-    <div className='button_top' hidden onClick={(e)=> handleScroll(e)}>
+    <div
+      className='button_top'
+      hidden
+      ref={btnRef}
+      onClick={(e)=> smoothlyTop(e)}
+    >
       <a href='#top' className='but_top' style={{backgroundImage: `url(images/svg/arrowTop.svg)`}}> </a>
     </div>
   );
 }
 
 export default ButtonTop;
+
